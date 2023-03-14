1bd984b8d73e1692669c5440e5004aa3b6368bf5
san292 <sance1921@gmail.com>
2023-03-14 11:11:00 +0100

    add button header components

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,24 @@
+import { Task } from "./Task";
+
+
+export const TASKS: Task[] = [
+
+  {
+    id: 1,
+    text: 'Doctors',
+    day: 'Mars 21th at 17:30',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Meeting michelin',
+    day: 'Mars 22th at 14:30',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Meeting EuroNews',
+    day: 'Mars21th at 10:30',
+    reminder: false,
+  },
+];
