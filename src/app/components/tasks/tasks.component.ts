f3ce28bfae903909c0c2dd0fa6c2c015f112b081
san292 <sance1921@gmail.com>
2023-03-16 10:14:02 +0100

    delete task

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -11,9 +11,13 @@ export class TasksComponent implements OnInit {
   tasks: Task[] = [];
   constructor(private taskService: TaskService) {}
   ngOnInit(): void {
-    console.log('taskService',TaskService );
-    
-
-    this.taskService.getTasks().subscribe((tasks)=>(this.tasks=tasks))
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
+  deleteTask(task: Task) {
+    this.taskService
+      .deleteTask(task)
+      .subscribe(
+        () => (this.tasks = this.tasks.filter((t: Task) => t.id !== task.id))
+      );
   }
 }
